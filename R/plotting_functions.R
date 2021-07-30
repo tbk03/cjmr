c9e850143f9eb043c5384578d9b6bb0475d27020
Chris Martin <chrisjmartin03@gmail.com>
2021-07-30 20:44:37 +0100

    working on a new data visualisation template Rmd file

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -5,3 +5,56 @@ test <-  function(){
 test2 <-  function(){
   print("cjmr test function 2")
 }
+
+explanatory_theme <- function(){
+
+
+  grid_line_colour <- "grey"
+  text_colour <- "black"
+  background_colour <- "grey98"
+
+  list(
+    ggplot2::theme_light(),
+
+    ggplot2::theme(
+
+      # format text
+      text = ggplot2::element_text(size = 14),
+      plot.title = ggplot2::element_text(hjust = 0.5,
+                                         size = 18),
+      plot.subtitle = ggplot2::element_text(hjust = -0.05,
+                                            size = 14),
+      plot.caption = ggplot2::element_text(size = 12, hjust = 0),
+
+      # format legend
+      legend.position = "top",
+      legend.background = ggplot2::element_blank(),
+      legend.title = ggplot2::element_blank(),
+
+      # format axis
+      axis.title.y = ggplot2::element_blank(),
+      axis.line.y = ggplot2::element_blank(),
+      axis.line.x = ggplot2::element_line(colour = "black", size = 1),
+      axis.ticks.x = ggplot2::element_line(colour = "black", size = 1),
+      axis.text.x = ggplot2::element_text(margin=ggplot2::margin(t = 7.5, b = 10)),
+
+      # format plot gridlines
+      panel.grid.minor = ggplot2::element_blank(),
+      panel.grid.major.x = ggplot2::element_blank(),
+      panel.grid.major.y = ggplot2::element_line(colour = grid_line_colour),
+
+      # format plot background
+      panel.background = ggplot2::element_blank(),
+
+      # format overall background (i.e. background behind the entire figure
+      # including the plot, titles etc.)
+      plot.background = element_rect(fill = background_colour),
+
+      # facet labels background
+      strip.background = ggplot2::element_rect(fill=background_colour),
+      strip.text = ggplot2::element_text(colour = text_colour, face = "bold",
+                                         size = 14),
+      panel.border = ggplot2::element_blank()
+    )
+  )
+}
---
+++ R/updated_plotting_functions.R
@@ -0,0 +1,71 @@
+# based on the bbc ggplot2 theme
+# https://bbc.github.io/rcookbook/
+
+# tweaks to add insights from Cédric Scherer (ggplot wizardry)
+# https://z3tt.github.io/OutlierConf2021/#left-aligned-title
+
+
+theme_cjmr_explanatory <- function(){
+
+  grid_line_colour <- "grey80"
+  text_colour <- "black"
+  background_colour <- "grey98"
+
+
+  ggplot2::theme_bw() %+replace%
+
+    ggplot2::theme(
+
+      # format text
+      text = ggplot2::element_text(family = "Lato", size = 12),
+      plot.title = ggplot2::element_text(hjust = 0,size = 16),
+      plot.subtitle = ggplot2::element_text(hjust = 0, size = 12),
+      plot.caption = ggplot2::element_text(size = 8,
+                                           hjust = 0),
+
+      # format legend
+      legend.position = "top",
+      legend.background = ggplot2::element_blank(),
+      legend.title = ggplot2::element_blank(),
+      legend.text = ggplot2::element_text(size = 10),
+
+      # format axis
+      #axis.text = ggplot2::element_text(face = "bold"),
+      axis.title.x = ggplot2::element_text(),
+      axis.title.y = ggplot2::element_blank(),
+      axis.line.y = ggplot2::element_blank(),
+      axis.line.x = ggplot2::element_blank(),
+      # axis.line.x = ggplot2::element_line(colour = "black", size = 1),
+      #axis.ticks.x = ggplot2::element_line(colour = grid_line_colour, size = 1),
+      axis.ticks = ggplot2::element_blank(),
+      axis.text.x = ggplot2::element_text(margin=ggplot2::margin(t = 1, b = 10)),
+
+      # format plot gridlines
+      panel.grid.minor = ggplot2::element_blank(),
+      panel.grid.major.x = ggplot2::element_blank(),
+      panel.grid.major.y = ggplot2::element_line(colour = grid_line_colour),
+
+      # format plot background
+      panel.background = ggplot2::element_blank(),
+
+      # format overall background (i.e. background behind the entire figure
+      # including the plot, titles etc.)
+      plot.background = ggplot2::element_blank(),
+
+      # facet labels background
+      strip.background = ggplot2::element_rect(fill=background_colour),
+      strip.text = ggplot2::element_text(colour = text_colour, face = "bold",
+                                         size = 12),
+      panel.border = ggplot2::element_blank(),
+
+      # alignment of text around the the plot
+      plot.title.position = "plot",
+      plot.caption.position = "plot"
+
+    )
+}
+
+clean_axis <- function(){
+    # limit expansion and turn clipping off
+    coord_cartesian(expand = FALSE, clip = "off")
+}
